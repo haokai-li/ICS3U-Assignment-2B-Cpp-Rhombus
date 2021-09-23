2b511db9ee2b37214c21a94e88690b0660925f4e
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-23 03:00:32 +0000

    rhombus

---
+++ rhombus.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This Program calculate the area and perimeter of the rhombus
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    // This Program calculate the area and perimeter of the rhombus
+
+    float sides;
+    float diagonal;
+    float anotherDiagonal;
+    float area;
+    float perimeter;
+
+    // input
+    std::cout << "Enter the side of the rhombus(cm): ";
+    std::cin >> sides;
+    std::cout << "Enter the diagonal of the rhombus(cm): ";
+    std::cin >> diagonal;
+    std::cout << "Enter the another diagonal of the rhombus(cm): ";
+    std::cin >> anotherDiagonal;
+    std::cout << "" << std::endl;
+
+    // process
+    area = (diagonal * anotherDiagonal) / 2;
+    perimeter = 4 * sides;
+
+    // output
+    std::cout << "a = (diagonal x another diagonal) / 2 = "
+    << area << " cm²" << std::endl;
+    std::cout << "p = 4 x sides = " << perimeter << " cm" << std::endl;
+    std::cout << "\nDone" << std::endl;
+}
