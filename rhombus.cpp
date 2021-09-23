94f7e9ce60eb3a7337550243f79fb4f8c1ccf76f
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-23 23:16:48 +0000

    revise

--- rhombus.cpp
+++ rhombus.cpp
@@ -12,7 +12,7 @@ int main() {
 
     float sides;
     float diagonal;
-    float anotherDiagonal;
+    float otherDiagonal;
     float area;
     float perimeter;
 
@@ -21,16 +21,16 @@ int main() {
     std::cin >> sides;
     std::cout << "Enter the diagonal of the rhombus(cm): ";
     std::cin >> diagonal;
-    std::cout << "Enter the another diagonal of the rhombus(cm): ";
-    std::cin >> anotherDiagonal;
+    std::cout << "Enter the other diagonal of the rhombus(cm): ";
+    std::cin >> otherDiagonal;
     std::cout << "" << std::endl;
 
     // process
-    area = (diagonal * anotherDiagonal) / 2;
+    area = (diagonal * otherDiagonal) / 2;
     perimeter = 4 * sides;
 
     // output
-    std::cout << "a = (diagonal x another diagonal) / 2 = "
+    std::cout << "a = (diagonal x other diagonal) / 2 = "
     << area << " cm²" << std::endl;
     std::cout << "p = 4 x sides = " << perimeter << " cm" << std::endl;
     std::cout << "\nDone" << std::endl;
